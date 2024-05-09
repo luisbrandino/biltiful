5a936d85d8f6fcc5a402be99bca920e0982785e9
luisbrandino <luisbrandino.contato@gmail.com>
2024-05-09 15:46:37 -0300

    feat: implementado o corpo da classe produto

---
+++ biltiful/Classes/Produto.cs
@@ -0,0 +1,44 @@
+﻿namespace biltiful.Classes
+{
+    internal class Produto
+    {
+        public string CodigoBarras {  get; set; }
+        public string Nome { get; set; }
+        public double ValorVenda { get; set; }
+        public DateOnly UltimaVenda { get; set; }
+        public DateOnly DataCadastro { get; set; }
+        public char Situacao { get; set; }
+
+        /**
+         *  Construtor para criar o objeto a partir da linha vinda do arquivo
+         */
+        public Produto(string dados)
+        {
+
+        }
+
+        /**
+         *  Construtor para criar o objeto com as propriedades diretamente 
+         */
+        public Produto(string codigoBarras, string nome, int valorVenda, DateOnly ultimaVenda, DateOnly dataCadastro, )
+        {
+
+        }
+
+        /**
+         *  Esse método verifica se o código de barras é válido de acordo com o padrão EAN-13
+         */
+        static bool VerificarCodigoBarras(string codigoDeBarras)
+        {
+            return true;
+        }
+
+        /**
+         *  Esse método transforma o objeto atual em uma linha que pode ser escrita
+         */
+        public string FormatarParaArquivo()
+        {
+            return string.Empty;
+        }
+    }
+}
