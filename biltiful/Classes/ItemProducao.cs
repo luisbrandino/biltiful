4748b8b83ea66c4929ea5172815723a2dcc8563e
thavao <othavioact@gmail.com>
2024-05-09 16:05:55 -0300

    Criando as classes Producao e ItemProducao

---
+++ biltiful/Classes/ItemProducao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biltiful.Classes
+{
+    internal class ItemProducao
+    {
+        public int Id { get; set; }
+        public DateOnly DataProducao { get; set; }
+        public int MateriaPrima { get; set; }
+        public int QuantidadeMateriaPrima { get; set; }
+
+        public ItemProducao()
+        {
+
+        }
+    }
+}
---
+++ biltiful/Classes/Producao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biltiful.Classes
+{
+    internal class Producao
+    {
+        public int Id { get; set; }
+        public DateOnly DataProducao { get; set; }
+        public int Produto { get; set; }
+        public double Quantidade { get; set; }
+
+        public Producao() { }
+    }
+}
