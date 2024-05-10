dcd59d027fa2e4e0167f600e25d92d36e8586c63
luisbrandino <luisbrandino.contato@gmail.com>
2024-05-10 14:33:30 -0300

    feat: implementado menu de operações para cada classe

--- biltiful/Modulos/ModuloCadastro.cs
+++ biltiful/Modulos/ModuloCadastro.cs
@@ -2,14 +2,87 @@
 {
     internal class ModuloCadastro
     {
+        void Clientes()
+        {
+            Menu menu = CriarMenuDeOperacao("Clientes");
+
+            menu.Perguntar();
+        }
+
+        void Produtos()
+        {
+            Menu menu = CriarMenuDeOperacao("Produtos");
+
+            menu.Perguntar();
+        }
+
+        void MateriasPrima()
+        {
+            Menu menu = CriarMenuDeOperacao("Matérias-prima");
+
+            menu.Perguntar();
+        }
+
+        void Fornecedores()
+        {
+            Menu menu = CriarMenuDeOperacao("Fornecedores");
+
+            menu.Perguntar();
+        }
+
+        void Inadimplentes()
+        {
+            Menu menu = CriarMenuDeOperacao("Inadimplentes");
+
+            menu.Perguntar();
+        }
+
+        void Bloqueados()
+        {
+            Menu menu = CriarMenuDeOperacao("Bloqueados");
+
+            menu.Perguntar();
+        }
+
+        Menu CriarMenuDeOperacao(string entidade)
+        {
+            Menu menu = new("Cadastrar", "Localizar", "Editar", "Impressão por registro", "Voltar");
+            menu.LimparAposImpressao(true);
+            menu.DefinirTitulo($">> {entidade.ToUpper()} <<");
+
+            return menu;
+        }
 
         public void Executar()
         {
-            Menu menu = new Menu("Clientes", "Produtos", "Matérias-prima", "Fornecedores", "Inadimplentes", "Bloqueados");
+            Menu menu = new Menu("Clientes", "Produtos", "Matérias-prima", "Fornecedores", "Inadimplentes", "Bloqueados", "Voltar");
 
-            menu.DefinirTitulo("MENU DE CADASTROS");
+            menu.DefinirTitulo(">> MENU DE CADASTROS <<");
 
-            menu.Perguntar();
+            switch (menu.Perguntar())
+            {
+                case 1:
+                    Clientes();
+                    break;
+                case 2:
+                    Produtos();
+                    break;
+                case 3:
+                    MateriasPrima();
+                    break;
+                case 4:
+                    Fornecedores();
+                    break;
+                case 5:
+                    Inadimplentes();
+                    break;
+                case 6:
+                    Bloqueados();
+                    break;
+                default:
+                    return;
+
+            }
         }
 
     }
