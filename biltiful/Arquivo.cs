5c720e50df01f7c3f5611d4901087db6a3a954d6
luisbrandino <luisbrandino.contato@gmail.com>
2024-05-10 11:18:57 -0300

    feat: implementação do corpo da classe Arquivo, responsável por manusear os arquivos

---
+++ biltiful/Arquivo.cs
@@ -0,0 +1,47 @@
+﻿namespace biltiful
+{
+    internal class Arquivo<T> where T : class
+    {
+        public string Caminho { get; private set; }
+
+        /**
+         *  Construtor recebe o caminho final apontado para o arquivo diretamente
+         */
+        public Arquivo(string caminhoFinal)
+        {
+            Caminho = caminhoFinal;
+        }
+
+        /**
+         *  Construtor recebe o caminho junto do arquivo a ser apontado
+         */
+        public Arquivo(string caminho, string arquivo)
+        {
+            Caminho = caminho + arquivo;
+        }
+
+        /**
+         *  Lê o arquivo retornando uma lista do tipo especificado, nulo se nada for encontrado no arquivo
+         */
+        public List<T>? Ler()
+        {
+            return null;
+        }
+
+        /**
+         *  Sobrescreve o arquivo atual com a nova lista do tipo especificado passado no parâmetro
+         */
+        public void Sobrescrever(List<T> entidades)
+        {
+
+        }
+
+        /**
+         *  Insere uma nova entrada no arquivo, sem sobrescrever 
+         */
+        public void Inserir(T entidade)
+        {
+        }
+
+    }
+}
