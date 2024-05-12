f0fb198eff2c8ced90bdea2fc515285c2cc25099
luisbrandino <luisbrandino.contato@gmail.com>
2024-05-12 15:23:37 -0300

    feat: criação das classes Inadimplente e Bloqueado

---
+++ biltiful/Classes/Bloqueado.cs
@@ -0,0 +1,33 @@
+﻿namespace biltiful.Classes
+{
+    internal class Bloqueado : IEntidade
+    {
+        public string CNPJ { get; private set; }
+
+        public Bloqueado()
+        {
+
+        }
+
+        public Bloqueado(string cnpj)
+        {
+            if (!Fornecedor.VerificarCNPJ(cnpj))
+                throw new ArgumentException("CNPJ informado é inválido");
+
+            LinhaParaObjeto(cnpj);
+        }
+
+        public string FormatarParaArquivo()
+        {
+            return CNPJ;
+        }
+
+        public void LinhaParaObjeto(string linha)
+        {
+            if (linha.Length != 14)
+                throw new ArgumentException("Linha não possui o tamanho padrão para a entidade Bloqueado");
+
+            CNPJ = linha.Substring(0, 14);
+        }
+    }
+}
