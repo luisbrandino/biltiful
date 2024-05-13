59eb57baf62b89bc98f9195f5d6522b53c270721
luisbrandino <luisbrandino.contato@gmail.com>
2024-05-12 22:32:33 -0300

    feat: operação de exclusão de venda finalizada

--- biltiful/Modulos/ModuloVenda.cs
+++ biltiful/Modulos/ModuloVenda.cs
@@ -1,21 +1,77 @@
 ﻿using biltiful.Classes;
 using biltiful.Modulos.Operacoes;
+using biltiful.Modulos.Operacoes.Entradas;
 
 namespace biltiful.Modulos
 {
     internal class ModuloVenda
     {
-        
+        Arquivo<ItemVenda> arquivoItemVendas = new Arquivo<ItemVenda>(Constantes.DIRETORIO, Constantes.ITEM_VENDA_ARQUIVO);
+        List<ItemVenda> itemVendas;
+        Arquivo<Venda> arquivoVendas = new Arquivo<Venda>(Constantes.DIRETORIO, Constantes.VENDA_ARQUIVO);
+        List<Venda> vendas;
+
+        public ModuloVenda()
+        {
+            Resetar();
+        }
+
+        void Resetar()
+        {
+            itemVendas = arquivoItemVendas.Ler();
+            vendas = arquivoVendas.Ler();
+        }
+
+        int EntradaId()
+        {
+            Entrada<int> entrada = new();
+
+            entrada.AdicionarRegra(
+                (int id) => vendas.Exists(v => v.Id == id),
+                "ID não registrado"
+            );
+
+            return entrada.Pegar();
+        }
+
 
         void Excluir()
         {
-            Arquivo<Venda> arquivoVendas = new Arquivo<Venda>(Constantes.DIRETORIO, Constantes.VENDA_ARQUIVO);
-            List<Venda> vendas = arquivoVendas.Ler();
+            if (vendas.Count <= 0)
+            {
+                Console.WriteLine("Nenhuma venda cadastrada!");
+                Console.ReadKey();
+                return;
+            }
 
-            Arquivo<ItemVenda> arquivoItemVendas = new Arquivo<ItemVenda>(Constantes.DIRETORIO, Constantes.ITEM_VENDA_ARQUIVO);
-            List<ItemVenda> itemVendas = arquivoItemVendas.Ler(); ;
+            Console.Write("Informe o ID da venda: ");
+            int id = EntradaId();
+
+            /**
+             *  Remove a venda selecionada pelo ID da lista de vendas (nunca será nulo pois foi checado sua existência no EntradaId)
+             */
+            vendas.Remove(vendas.Find(v => v.Id == id));
 
             List<ItemVenda> itensParaExcluir = new List<ItemVenda>();
+
+            /**
+             *  Adicione os itens da compra relacionados com o ID para uma lista auxiliar para exclusão
+             */
+            foreach (ItemVenda item in itemVendas)
+                if (item.Id == id)
+                    itensParaExcluir.Add(item);
+
+            /**
+             *  Remove os itens selecionados pelo ID da lista antes da sobrescrita do arquivo
+             */
+            foreach(ItemVenda item in itensParaExcluir)
+                itemVendas.Remove(item);
+
+            /**
+             *  Sobrescreve os arquivos de fato com a nova lista que exclui os itens selecionados
+             */
+            arquivoItemVendas.Sobrescrever(itemVendas);
+            arquivoVendas.Sobrescrever(vendas);
         }
 
         void ImpressaoPorRegistro()
@@ -41,6 +97,7 @@ namespace biltiful.Modulos
 
             while (true)
             {
+                Resetar();
                 switch (menu.Perguntar())
                 {
                     case 1:
@@ -48,6 +105,7 @@ namespace biltiful.Modulos
                     case 2:
                         break;
                     case 3:
+                        Excluir();
                         break;
                     case 4:
                         ImpressaoPorRegistro();
