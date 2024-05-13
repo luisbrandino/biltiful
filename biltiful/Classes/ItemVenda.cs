064ffc47ede6ecb0f93bd296d038361f74555208
thavao <othavioact@gmail.com>
2024-05-12 21:50:43 -0300

    IEntidade emplementada

---
+++ biltiful/Classes/ItemVenda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biltiful.Classes
+{
+    internal class ItemVenda : IEntidade
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public int Quantidade { get; set; }
+        public float ValorUnitario { get; set; }
+        public float TotalItem { get; set; }
+
+        public string FormatarParaArquivo()
+        {
+            string id, quantidade, valorUnitario, totalItem;
+            id = this.Id.ToString().PadLeft(5, '0');
+            quantidade = this.Quantidade.ToString();
+            valorUnitario = this.ValorUnitario.ToString().Replace(",", "").PadLeft(5, '0');
+            totalItem = this.TotalItem.ToString().Replace(",", "").PadLeft(7, '0');
+
+            return id + this.Produto + quantidade + valorUnitario + totalItem;
+
+        }
+
+        public void LinhaParaObjeto(string linha)
+        {
+            this.Id = int.Parse(linha.Substring(0, 5).Trim());
+            this.Produto = linha.Substring(5, 13).Trim();
+            this.Quantidade = int.Parse(linha.Substring(18, 3).Trim());
+            this.ValorUnitario = float.Parse(linha.Substring(21, 5).Trim());
+            this.TotalItem = float.Parse(linha.Substring(26, 6).Trim());
+
+        }
+    }
+}
---
+++ biltiful/Classes/Venda.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biltiful.Classes
+{
+    internal class Venda : IEntidade
+    {
+        public int Id { get; set; }
+        public DateOnly DataVenda { get; set; }
+        public string Cliente { get; set; }
+        public float ValorTotal { get; set; }
+
+        public Venda() { }
+
+        public Venda(int id, DateOnly dataVenda, string cliente, float valorTotal)
+        {
+            Id = id;
+            DataVenda = dataVenda;
+            Cliente = cliente;
+            ValorTotal = valorTotal;
+        }
+
+        public string FormatarParaArquivo()
+        {
+            string id, data, valorTotal;
+            id = this.Id.ToString().PadLeft(5, '0');
+            data = this.DataVenda.ToString().Replace("/", "");
+            valorTotal = this.ValorTotal.ToString().Replace(",", "").PadLeft(7, '0');
+
+            return id + data + this.Cliente + valorTotal;
+        }
+
+        public void LinhaParaObjeto(string linha)
+        {
+            this.Id= int.Parse(linha.Substring(0, 5).Trim());
+            int dia = int.Parse(linha.Substring(5, 2).Trim());
+            int mes = int.Parse(linha.Substring(7, 2).Trim());
+            int ano = int.Parse(linha.Substring(9, 4).Trim());
+            this.DataVenda = new DateOnly(ano, mes, dia);
+            this.Cliente = linha.Substring(13, 11).Trim();
+            this.ValorTotal = float.Parse(linha.Substring(24, 7).Insert(5, ","));
+        }
+    }
+}
